2e15873c8e6b5ad9c9572915145c5352c921517b
Samuel <samuel.s.sudhakar@gmail.com>
2023-03-21 10:09:53 -0400

    Added getters and setters to PuzzlePiece

--- src/BackEnd/PuzzlePiece.java
+++ src/BackEnd/PuzzlePiece.java
@@ -1,8 +1,28 @@
 package BackEnd;
 import java.awt.*;
+import javax.swing.*;
+import java.util.*;
 public class PuzzlePiece {
-    private Image sprite;
-    public PuzzlePiece () {
-        //Creates a random puzzle piece
+    private ImageIcon[] blocks = {};
+    private ImageIcon shape;
+    private int xCell;
+    private int yCell;
+    public PuzzlePiece(){
+        shape = blocks[(int)(Math.random())*blocks.length+1];
+    }
+    public ImageIcon getShape(){
+        return shape;
+    }
+    public void setX(int cell){
+        xCell = cell;
+    }
+    public int getX(){
+        return xCell;
+    }
+    public void setY(int cell){
+        yCell = cell;
+    }
+    public int getY(){
+        return yCell;
     }
 }
