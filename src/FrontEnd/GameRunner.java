70fc75da90e1a42ec17637e42d07a225c3e8c69c
MichaelMorton1 <ipibym@gmail.com>
2023-03-20 20:23:05 -0400

    Restructed the outline for the design.

---
+++ src/FrontEnd/GameRunner.java
@@ -0,0 +1,34 @@
+package FrontEnd;
+import javax.swing.*;
+
+import BackEnd.Game;
+
+public class GameRunner {
+    public static void playTetris() {
+        //This is the frame work for how the game will be structured
+        //My goal is to integrate MainMenu last.
+
+        JFrame frame = new JFrame("Tetris");
+        frame.setSize(800,800);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+
+        HumanPlayer human = new HumanPlayer(frame);
+        ComputerPlayer computer = new ComputerPlayer(frame);
+
+
+        //You would want to get both games, running at the same time
+        //Whichever game ends first 
+        Game game = new Game(human);
+        Game computerGame = new Game(computer);
+        
+        //This will be changed to run both games at once using threads.
+        //The next thread should be run in this class
+        int score; 
+        while (!game.gameOver()) {
+            score = game.start();
+        }
+        
+
+    }
+}
