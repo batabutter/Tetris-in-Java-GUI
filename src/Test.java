22c7dca4b0cbaf549349afc22fce22194d1a0e98
Samuel <samuel.s.sudhakar@gmail.com>
2023-03-22 10:20:19 -0400

    Added threads to run human and computer running simultaneously

--- src/Test.java
+++ src/Test.java
@@ -1,19 +1,20 @@
 import FrontEnd.GameRunner;
 import javax.swing.*;
 import java.awt.Point;
+import java.awt.*;
 import FrontEnd.Player;
 import FrontEnd.ComputerPlayer;
 import BackEnd.Game;
 
-public class Test {
+public class Test extends Thread{
+    static JFrame frame = new JFrame("Tetris");
     public static void main(String args[]) {
-        JFrame frame = new JFrame("Tetris");
+        Test thread = new Test();
         frame.setSize(1000,800);
+        frame.setBackground(Color.black);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        //Player human = new Player(100, 20, frame);
+        thread.start();
         ComputerPlayer computer=  new ComputerPlayer(600, 70, frame);
-
 
         //You would want to get both games, running at the same time
         //Whichever game ends first 
@@ -24,5 +25,10 @@ public class Test {
         Game game2 = new Game(computer);
         game2.start();
 
+    }
+    public void run(){
+        Player human = new Player(100,20,frame);
+        Game game = new Game(human);
+        game.start();
     }
 }
